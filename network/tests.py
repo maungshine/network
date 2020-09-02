4330433b74b74f6e6e9b2aa6d0c6d4c46063bd02
Maung Shine <34064304+maungshine@users.noreply.github.com>
2020-09-03 00:04:31 +0630

    design better to remove repetitive code

--- network/tests.py
+++ network/tests.py
@@ -9,22 +9,15 @@ class NetworkTestCase(TestCase):
         user2 = User.objects.create(username="koko",email="koko@example.com", password="kokopassword")
 
         #create user posts
-        post1 = Post.objects.create(post_user=user1, content="This is user1's post1.")
-        Post.objects.create(post_user=user1, content="This is user1's post2.")
-        Post.objects.create(post_user=user1, content="This is user1's post3.")
-        Post.objects.create(post_user=user1, content="This is user1's post4.")
-        Post.objects.create(post_user=user1, content="This is user1's post5.")
-        Post.objects.create(post_user=user1, content="This is user1's post6.")
-        post7 = Post.objects.create(post_user=user2, content="This is user2's post1.")
-        Post.objects.create(post_user=user2, content="This is user2's post2.")
-        Post.objects.create(post_user=user2, content="This is user2's post3.")
-        Post.objects.create(post_user=user2, content="This is user2's post4.")
-        Post.objects.create(post_user=user2, content="This is user2's post5.")
-        Post.objects.create(post_user=user2, content="This is user2's post6.")
+        for post_num in range(11):
+            Post.objects.create(post_user=user1, content=f"This is user1's post{post_num}.")
+            Post.objects.create(post_user=user2, content=f"This is user2's post{post_num}.")
 
 
         #create likes
-        Like.objects.create(liked_user=user1, liked_post=post7)
+        post1 = Post.objects.get(pk=1)
+        post2 = Post.objects.get(pk=2)
+        Like.objects.create(liked_user=user1, liked_post=post2)
         Like.objects.create(liked_user=user2, liked_post=post1)
 
         #create following
@@ -40,13 +33,6 @@ class NetworkTestCase(TestCase):
 
     def test_following(self):
         """check if response of login user is True and paginate 10posts per following page"""
-        user1 = User.objects.get(pk=1)
-        user2 = User.objects.get(pk=2)
-        for post_num in range(13,18):
-            Post.objects.create(post_user=user1, content=f"This is user1's post{post_num}.")
-        for post_num in range(18,23):
-            Post.objects.create(post_user=user2, content=f"This is user2's post{post_num}.")
-
         c = Client()
         login = c.login(username="mgmg", password="mgmgpassword")
         self.assertTrue(login)
